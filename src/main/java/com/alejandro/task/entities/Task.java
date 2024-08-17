95a114b3d1d0891be60a206e0a2bbb69df5b9641
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-17 19:52:56 -0400

    se creo la entidad principal de la tarea

---
+++ src/main/java/com/alejandro/task/entities/Task.java
@@ -0,0 +1,23 @@
+package com.alejandro.task.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+}
