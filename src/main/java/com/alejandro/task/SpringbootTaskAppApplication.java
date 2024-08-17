87ad7adf2a9bfaae8a4c6bf2a7cf0daee27c5900
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-17 19:48:16 -0400

    initial commit

---
+++ src/main/java/com/alejandro/task/SpringbootTaskAppApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTaskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTaskAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejandro/task/SpringbootTaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
