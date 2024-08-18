3f0e7d736c23c9206ed7a52aada745ad25dca2f0
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-17 20:34:42 -0400

    fix

--- src/main/java/com/alejandro/task/repositories/TaskRepository.java
+++ src/main/java/com/alejandro/task/repositories/TaskRepository.java
@@ -2,6 +2,8 @@ package com.alejandro.task.repositories;
 
 import com.alejandro.task.entities.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 }
